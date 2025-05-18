ef6f9c67eeca13103e5e1c340b01c88fc8f6e200
Matej <hozlarmatej0@gmail.com>
2025-05-18 17:18:50 +0200

    readme fix

--- client/src/assets/potatos/potatos.js
+++ client/src/assets/potatos/potatos.js
@@ -1,9 +1,1 @@
-const potatoes = {
-  potato1: require("./potato1.png"),
-  potato2: require("./potato2.png"),
-  potato3: require("./potato3.png"),
-  potato4: require("./potato4.png"),
-  potato5: require("./potato5.png"),
-};
 
-export default potatoes;
